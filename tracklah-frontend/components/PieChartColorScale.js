35f9d573c87cd907b9ed985db5b26dec16127551
m-chocolate <94664208+m-chocolate@users.noreply.github.com>
2022-04-20 14:38:47 +0800

    Separate Chart Files

---
+++ tracklah-frontend/components/PieChartColorScale.js
@@ -0,0 +1,25 @@
+const colorScale= [
+    "lightskyblue",
+    "tomato", 
+    "hotpink", 
+    "orange", 
+    "green", 
+    "navy", 
+    "gold",
+    "brown", 
+    "red",
+    "purple",
+    "cyan", 
+    "rosybrown",
+    "plum",
+    "chocolate",
+    "silver",
+    "mediumaquamarine",
+    "olive",
+    "lawngreen",
+    "coral",
+    "teal"
+    //one colour per category. total 20
+]
+
+export default colorScale;
