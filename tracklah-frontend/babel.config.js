6668fbfd6855c2932f08d311a3b0c7313efed9a8
QuackZ90 <82066443+QuackZ90@users.noreply.github.com>
2022-04-20 15:41:48 +0800

    resolved conflict

--- tracklah-frontend/babel.config.js
+++ tracklah-frontend/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
     plugins: ['react-native-reanimated/plugin', ["module:react-native-dotenv"]],
-=======
-    plugins: [['react-native-reanimated/plugin'], ["module:react-native-dotenv"]]
->>>>>>> main
   };
 };
