a186fb313d129b3dd16fcef3881cc0e6694dde21
Michelle Tay <mich.tay.elle@gmail.com>
2022-04-28 16:49:10 +0800

    Edit ConversionInput and setup CurrencyConverter Screen

---
+++ tracklah-frontend/styles/CurrencyConverter-styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.beige,
+        flex: 1,
+        justifyContent: "center"
+    },
+});
+ 
+export default styles;
